ce23331b84817b67952c0a8b806e124ba427d99a
imabwahab <imabwahab56@gmail.com>
2026-05-31 09:07:04 +0500

    Update Id columns to NVARCHAR(50) and enforce NOT NULL
    Modified the `Id` columns in the `Accounts`, `Categories`, and
    `Transactions` tables to use the `NVARCHAR(50)` data type, which
    supports Unicode and has a maximum length of 50. Added the
    `NOT NULL` constraint to ensure data integrity by preventing
    null values in these columns.

---
+++ Databases/SQLQuery2.sql
@@ -0,0 +1,11 @@
+﻿-- For Accounts table
+ALTER TABLE Accounts
+ALTER COLUMN Id NVARCHAR(50) NOT NULL;
+
+-- For Categories table
+ALTER TABLE Categories
+ALTER COLUMN Id NVARCHAR(50) NOT NULL;
+
+-- For Transactions table
+ALTER TABLE Transactions
+ALTER COLUMN Id NVARCHAR(50) NOT NULL;
